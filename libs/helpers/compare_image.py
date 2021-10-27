ec08555c3523af2be23332f219ee7b66a3ae3811
kolamb <isrLamb@outlook.com>
2021-10-27 18:29:44 +0300

    refactor subsystem compare_image.py for readability

--- libs/helpers/compare_image.py
+++ libs/helpers/compare_image.py
@@ -13,9 +13,81 @@ from libs.helpers.helper import *
 
 class CompareImage:
 
-    def __init__(self, file_name, helper, koff=98):
+    def __init__(self, converted_file, helper, koff=98):
         self.helper = helper
-        self.start_to_compare_images(file_name, koff)
+        self.converted_file = converted_file
+        self.screen_folder = 'screen'
+        self.source_file = converted_file.replace(f'.{self.helper.converted_extension}',
+                                                  f'.{self.helper.source_extension}')
+        self.folder_name = converted_file.replace(f'.{converted_file.split(".")[-1]}', '')
+        self.folder_name = re.sub(r"^\s+|\n|\r|\s|\s+$", '', self.folder_name)
+        Helper.create_dir(f'{self.helper.passed}{self.folder_name}')
+        Helper.create_dir(f'{self.helper.passed}{self.folder_name}/{self.screen_folder}')
+        self.start_to_compare_images(koff)
+
+    def start_to_compare_images(self, koff):
+        for image_name in track(os.listdir(self.helper.tmp_dir_converted_image), description='Comparing In Progress'):
+            if os.path.exists(f'{self.helper.tmp_dir_source_image}{image_name}') \
+                    and os.path.exists(f'{self.helper.tmp_dir_converted_image}{image_name}'):
+
+                self.compare_img(f'{self.helper.tmp_dir_source_image}{image_name}',
+                                 f'{self.helper.tmp_dir_converted_image}{image_name}',
+                                 image_name,
+                                 koff)
+
+            else:
+                print(f'[bold red] File not found [/bold red]{image_name}')
+
+        Helper.delete(f'{self.helper.tmp_dir_converted_image}*')
+        Helper.delete(f'{self.helper.tmp_dir_source_image}*')
+
+    def compare_img(self, image_before_conversion, image_after_conversion, image_name, koff):
+        image_name = image_name.split('.')[0]
+        sheet = image_name.split('_')[-1]
+        file_name_for_gif = f'{image_name}_similarity.gif'
+        before_full = cv2.imread(image_before_conversion)
+        after_full = cv2.imread(image_after_conversion)
+        collage = self.collage(after_full, before_full)
+
+        if self.converted_file.endswith((".xlsx", ".XLSX")):
+            after, before, similarity = self.find_difference(after_full, before_full)
+        else:
+            before = self.find_contours(image_before_conversion)
+            after = self.find_contours(image_after_conversion)
+            try:
+                after, before, similarity = self.find_difference(after, before)
+            except Exception:
+                after, before, similarity = self.find_difference(after_full, before_full)
+
+        print(f"{self.converted_file} Sheet: {sheet} similarity: {similarity}")
+        before = self.put_text(before, f'Before sheet {sheet}. Similarity {round(similarity, 3)}%')
+        after = self.put_text(after, f'After sheet {sheet}. Similarity {round(similarity, 3)}%')
+        images = [before, after]
+        if similarity < koff:
+            self.write_down_results(collage, images, file_name_for_gif, sheet)
+        else:
+            print('[bold green]passed[/bold green]')
+            imageio.mimsave(f'{self.helper.passed}{self.folder_name}/{file_name_for_gif}',
+                            images,
+                            duration=1)
+            cv2.imwrite(
+                f'{self.helper.passed}{self.folder_name}/{self.screen_folder}/{self.converted_file}_{sheet}_collage.png',
+                collage)
+
+    def write_down_results(self, collage, images, file_name_for_gif, sheet):
+        self.helper.create_dir(f'{self.helper.differences_compare_image}{self.folder_name}')
+        self.helper.create_dir(f'{self.helper.differences_compare_image}{self.folder_name}/gif/')
+        self.helper.create_dir(f'{self.helper.differences_compare_image}{self.folder_name}/{self.screen_folder}')
+        self.helper.copy_to_folder(self.converted_file, self.source_file,
+                                   f'{self.helper.differences_compare_image}{self.folder_name}/')
+        cv2.imwrite(
+            f'{self.helper.differences_compare_image}{self.folder_name}/{self.screen_folder}/'
+            f'{self.converted_file}_{sheet}_collage.png',
+            collage)
+        imageio.mimsave(f'{self.helper.differences_compare_image}{self.folder_name}/gif/{file_name_for_gif}',
+                        images,
+                        duration=1)
+        pass
 
     @staticmethod
     def find_contours(img):
@@ -24,10 +96,7 @@ class CompareImage:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         _, binary = cv2.threshold(gray, 125, 255, cv2.THRESH_BINARY)
         contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        # x, y, w, h = cv2.boundingRect(contours)
         for c in contours:
-            # print(c)
-            # get the bounding rect
             x, y, w, h = cv2.boundingRect(c)
             if h >= 500:
                 # to save the images
@@ -36,38 +105,12 @@ class CompareImage:
             else:
                 return img_source
 
-    def compare_img(self, before_conversion, after_conversion, image_name, converted_file, koff):
-        folder_name = converted_file.replace(f'.{converted_file.split(".")[-1]}', '')
-        folder_name = re.sub(r"^\s+|\n|\r|\s|\s+$", '', folder_name)
-        screen_folder = 'screen'
-        Helper.create_dir(f'{self.helper.passed}{folder_name}')
-        Helper.create_dir(f'{self.helper.passed}{folder_name}/{screen_folder}')
-        image_name = image_name.split('.')[0]
-        sheet = image_name.split('_')[-1]
-        if converted_file.endswith((".xlsx", ".XLSX")):
-            before = cv2.imread(before_conversion)
-            after = cv2.imread(after_conversion)
-        else:
-            before = self.find_contours(before_conversion)
-            after = self.find_contours(after_conversion)
-
-        before_full = cv2.imread(before_conversion)
-        after_full = cv2.imread(after_conversion)
-        after_for_collage = self.put_text(after_full, f'After')
-        before_for_collage = self.put_text(before_full, f'Before')
-        collage = np.hstack([before_for_collage, after_for_collage])
-
-        try:
-            before_gray = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
-            after_gray = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
-            (score, diff) = structural_similarity(before_gray, after_gray, full=True)
-            diff = (diff * 255).astype("uint8")
-        except Exception:
-            before_gray = cv2.cvtColor(before_full, cv2.COLOR_BGR2GRAY)
-            after_gray = cv2.cvtColor(after_full, cv2.COLOR_BGR2GRAY)
-            (score, diff) = structural_similarity(before_gray, after_gray, full=True)
-            diff = (diff * 255).astype("uint8")
-
+    @staticmethod
+    def find_difference(after, before):
+        before_gray = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
+        after_gray = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
+        (score, diff) = structural_similarity(before_gray, after_gray, full=True)
+        diff = (diff * 255).astype("uint8")
         thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
         contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours = contours[0] if len(contours) == 2 else contours[1]
@@ -78,37 +121,14 @@ class CompareImage:
                 x, y, w, h = cv2.boundingRect(c)
                 cv2.rectangle(before, (x, y), (x + w, y + h), color, 0)
                 cv2.rectangle(after, (x, y), (x + w, y + h), color, 0)
-
         similarity = score * 100
+        return after, before, similarity
 
-        print(f"{converted_file} Sheet: {sheet} similarity: {similarity}")
-
-        before = self.put_text(before, f'Before sheet {sheet}. Similarity {round(similarity, 3)}%')
-        after = self.put_text(after, f'After sheet {sheet}. Similarity {round(similarity, 3)}%')
-
-        images = [before, after]
-
-        file_name_for_gif = f'{image_name}_similarity.gif'
-        source_file = converted_file.replace(f'.{self.helper.converted_extension}', f'.{self.helper.source_extension}')
-        if similarity < koff:
-            self.helper.create_dir(f'{self.helper.differences_compare_image}{folder_name}')
-            self.helper.create_dir(f'{self.helper.differences_compare_image}{folder_name}/gif/')
-            self.helper.create_dir(f'{self.helper.differences_compare_image}{folder_name}/{screen_folder}')
-            self.helper.copy_to_folder(converted_file, source_file,
-                                       f'{self.helper.differences_compare_image}{folder_name}/')
-            cv2.imwrite(
-                f'{self.helper.differences_compare_image}{folder_name}/{screen_folder}/{converted_file}_{sheet}_collage.png',
-                collage)
-            imageio.mimsave(f'{self.helper.differences_compare_image}{folder_name}/gif/{file_name_for_gif}',
-                            images,
-                            duration=1)
-        else:
-            imageio.mimsave(f'{self.helper.passed}{folder_name}/{file_name_for_gif}',
-                            images,
-                            duration=1)
-            cv2.imwrite(
-                f'{self.helper.differences_compare_image}{folder_name}/{screen_folder}/{converted_file}_{sheet}_collage.png',
-                collage)
+    def collage(self, image1, image2):
+        after_for_collage = self.put_text(image1, f'After')
+        before_for_collage = self.put_text(image2, f'Before')
+        collage = np.hstack([before_for_collage, after_for_collage])
+        return collage
 
     @staticmethod
     def put_text(image, text):
@@ -121,7 +141,6 @@ class CompareImage:
                     thickness=2)
 
         return image
-        pass
 
     @staticmethod
     def grab_coordinate_exel(path, filename, list_num, number_of_pages, coordinate):
@@ -140,21 +159,3 @@ class CompareImage:
         filename_for_screen = filename.replace(f'.{filename.split(".")[-1]}', '')
         im = ImageGrab.grab()
         im.save(path + filename_for_screen + str(f'_{number_of_pages}') + '.png', 'PNG')
-
-    def start_to_compare_images(self, file_name, koff):
-        list_of_images = os.listdir(self.helper.tmp_dir_converted_image)
-        for image_name in track(list_of_images, description='Comparing In Progress'):
-            if os.path.exists(f'{self.helper.tmp_dir_source_image}{image_name}') \
-                    and os.path.exists(f'{self.helper.tmp_dir_converted_image}{image_name}'):
-
-                self.compare_img(f'{self.helper.tmp_dir_source_image}{image_name}',
-                                 f'{self.helper.tmp_dir_converted_image}{image_name}',
-                                 image_name,
-                                 file_name,
-                                 koff)
-
-            else:
-                print(f'[bold red] File not found [/bold red]{image_name}')
-
-        Helper.delete(f'{self.helper.tmp_dir_converted_image}*')
-        Helper.delete(f'{self.helper.tmp_dir_source_image}*')
