defa169d023bf4bb1285b38760207fc28b77e7c4
kolamb <isrLamb@outlook.com>
2021-12-24 12:50:23 +0300

    refactored preparing files for test in xls-xlsx module

--- libs/functional/spreadsheets/xls_to_xlsx_statistic_compare.py
+++ libs/functional/spreadsheets/xls_to_xlsx_statistic_compare.py
@@ -24,13 +24,13 @@ class Excel:
         self.helper = Helper(source_extension, converted_extension)
         self.check_errors = CheckErrors()
         self.coordinate = []
+        self.statistics_excel = None
         self.click = self.helper.click
-        self.file_name_for_log = ''
         self.shell = Dispatch("WScript.Shell")
         logger.info(f'The {source_extension}_{converted_extension} comparison on version: {version} is running.')
 
     def get_excel_statistic(self, wb):
-        statistics_exel = {
+        self.statistics_excel = {
             'num_of_sheets': f'{wb.Sheets.Count}',
         }
         try:
@@ -40,34 +40,30 @@ class Excel:
                 used = ws.UsedRange
                 nrows = used.Row + used.Rows.Count - 1
                 ncols = used.Column + used.Columns.Count - 1
-                statistics_exel[f'{num_of_sheet}_page_name'] = sh.Name
-                statistics_exel[f'{num_of_sheet}_nrows'] = nrows
-                statistics_exel[f'{num_of_sheet}_ncols'] = ncols
+                self.statistics_excel[f'{num_of_sheet}_page_name'] = sh.Name
+                self.statistics_excel[f'{num_of_sheet}_nrows'] = nrows
+                self.statistics_excel[f'{num_of_sheet}_ncols'] = ncols
                 num_of_sheet += 1
-            return statistics_exel
 
         except Exception:
-            logger.error(f'\nFailed to get full statistics excel from file: {self.file_name_for_log}\n '
-                         f'statistics: {statistics_exel}')
-            return statistics_exel
+            logger.error(f'\nFailed to get full statistics excel from file: {self.helper.converted_file}\n '
+                         f'statistics: {self.statistics_excel}')
 
-    def opener_excel(self, path_for_open, file_name):
-        error_processing = Process(target=self.check_errors.run_get_error_exel, args=(self.file_name_for_log,))
+    def opener_excel(self, file_name):
+        error_processing = Process(target=self.check_errors.run_get_error_exel, args=(self.helper.converted_file,))
         error_processing.start()
         try:
             xl = Dispatch("Excel.Application")
             xl.Visible = False
-            wb = xl.Workbooks.Open(f'{path_for_open}{file_name}')
-            statistics_excel = self.get_excel_statistic(wb)
+            wb = xl.Workbooks.Open(f'{self.helper.tmp_dir_in_test}{file_name}')
+            self.get_excel_statistic(wb)
             wb.Close(False)
             xl.Quit()
-            return statistics_excel
 
         except Exception:
             error = traceback.format_exc()
-            logger.error(f'{error} happened while opening file: {self.file_name_for_log}')
-            statistics_excel = {}
-            return statistics_excel
+            logger.error(f'{error} happened while opening file: {self.helper.converted_file}')
+            self.statistics_excel = None
 
         finally:
             os.system("taskkill /t /im  EXCEL.EXE")
@@ -77,36 +73,33 @@ class Excel:
         with io.open('./report.csv', 'w', encoding="utf-8") as csvfile:
             writer = csv.writer(csvfile, delimiter=';')
             writer.writerow(['File_name', 'statistic'])
-            for converted_file in track(list_of_files,
-                                        description='[bold blue]Comparing Excel Statistic...[/bold blue]\n'):
-                if converted_file.endswith((".xlsx", ".XLSX")):
-                    source_file, tmp_name_converted_file, \
-                    tmp_name_source_file, tmp_name = self.helper.preparing_files_for_test(converted_file,
-                                                                                          converted_extension,
-                                                                                          source_extension)
+            for self.helper.converted_file in track(list_of_files,
+                                                    description='[bold blue]'
+                                                                'Comparing Excel Statistic...'
+                                                                '[/bold blue]'):
 
-                    self.file_name_for_log = converted_file
+                if self.helper.converted_file.endswith((".xlsx", ".XLSX")):
+                    self.helper.preparing_files_for_test()
 
-                    print(f'[bold green]In test[/bold green] {source_file} '
-                          f'[bold green]and[/bold green] {converted_file}')
-                    source_statistics = self.opener_excel(self.helper.tmp_dir_in_test, tmp_name_source_file)
-                    converted_statistics = self.opener_excel(self.helper.tmp_dir_in_test, tmp_name_converted_file)
+                    print(f'[bold green]In test[/bold green] {self.helper.source_file} '
+                          f'[bold green]and[/bold green] {self.helper.converted_file}')
 
-                    if source_statistics == {} or converted_statistics == {}:
+                    self.opener_excel(self.helper.tmp_name_source_file)
+                    source_statistics = self.statistics_excel
+                    self.opener_excel(self.helper.tmp_name_converted_file)
+                    converted_statistics = self.statistics_excel
+
+                    if source_statistics is None or converted_statistics is None:
                         print("[bold red]Can't open source file, copy to untested[/bold red]")
-                        self.helper.copy_to_folder(converted_file,
-                                                   source_file,
-                                                   self.helper.untested_folder)
+                        self.helper.copy_to_folder(self.helper.untested_folder)
 
                     else:
                         modified = self.helper.dict_compare(source_statistics, converted_statistics)
                         if modified != {}:
                             print(f'[bold red]Differences: {modified}[/bold red]')
-                            self.helper.copy_to_folder(converted_file,
-                                                       source_file,
-                                                       self.helper.differences_statistic)
+                            self.helper.copy_to_folder(self.helper.differences_statistic)
 
-                            modified_keys = [converted_file, modified]
+                            modified_keys = [self.helper.converted_file, modified]
                             writer.writerow(modified_keys)
 
             self.helper.tmp_cleaner()
