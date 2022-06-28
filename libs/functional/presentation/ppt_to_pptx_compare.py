a93722337828bbe2ec768bde403c8aab395a4942
kolamb <isrLamb@outlook.com>
2022-06-28 19:03:31 +0300

    refactoring presentation comparisons

--- libs/functional/presentation/ppt_to_pptx_compare.py
+++ libs/functional/presentation/ppt_to_pptx_compare.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import pyautogui as pg
 from loguru import logger
 from rich import print
 
@@ -18,45 +17,24 @@ class PptPptxCompareImg:
 
     def run_compare_pp(self, list_of_files):
         for self.helper.converted_file in list_of_files:
-            if self.helper.converted_file.endswith((".pptx", ".PPTX")):
-                self.helper.preparing_files_for_test()
+            if not self.helper.converted_file.endswith((".pptx", ".PPTX")):
+                continue
 
-                print(f'[bold green]In test[/] {self.helper.converted_file}')
-                self.power_point.opener_power_point(self.helper.tmp_dir_in_test, self.helper.tmp_name)
+            self.helper.preparing_files_for_test()
 
-                if self.power_point.slide_count is not None:
-                    self.power_point.open_presentation_with_cmd(self.helper.tmp_name_converted_file)
-                    if self.power_point.check_errors.errors \
-                            and self.power_point.check_errors.errors[0] == "#32770" \
-                            and self.power_point.check_errors.errors[1] == "Microsoft PowerPoint":
+            print(f'[bold green]In test[/] {self.helper.converted_file}')
+            if not self.power_point.opener_power_point(self.helper.tmp_dir_in_test, self.helper.tmp_name):
+                continue
+            self.power_point.open_presentation_with_cmd(self.helper.tmp_name_converted_file)
+            if not self.power_point.errors_handler_when_opening():
+                self.power_point.close_presentation_with_hotkey()
+                continue
+            self.power_point.get_screenshot(self.helper.tmp_dir_converted_image)
+            self.power_point.close_presentation_with_hotkey()
 
-                        logger.error(f"'an error has occurred while opening the file'. "
-                                     f"Copied files: {self.helper.converted_file} "
-                                     f"and {self.helper.source_file} to 'failed_to_open_converted_file'")
-
-                        pg.press('esc', presses=3, interval=0.2)
-                        self.power_point.close_presentation_with_hotkey()
-                        self.helper.copy_to_folder(self.helper.untested_folder)
-                        self.power_point.check_errors.errors.clear()
-
-                    elif not self.power_point.check_errors.errors:
-                        self.power_point.get_screenshot(self.helper.tmp_dir_converted_image)
-                        self.power_point.close_presentation_with_hotkey()
-
-                        print(f'[bold green]In test[/] {self.helper.source_file}')
-                        self.power_point.open_presentation_with_cmd(self.helper.tmp_name_source_file)
-                        self.power_point.get_screenshot(self.helper.tmp_dir_source_image)
-                        self.power_point.close_presentation_with_hotkey()
-
-                        CompareImage(self.helper)
-
-                    else:
-                        logger.debug(f"Error message: {self.power_point.check_errors.errors} "
-                                     f"Filename: {self.helper.converted_file}")
-
-                else:
-                    logger.error(f"Can't open {self.helper.source_file}. Copied files"
-                                 " to 'failed_to_open_file'")
-
-                    self.helper.copy_to_folder(self.helper.failed_source)
+            print(f'[bold green]In test[/] {self.helper.source_file}')
+            self.power_point.open_presentation_with_cmd(self.helper.tmp_name_source_file)
+            self.power_point.get_screenshot(self.helper.tmp_dir_source_image)
+            self.power_point.close_presentation_with_hotkey()
+            CompareImage(self.helper)
             self.helper.tmp_cleaner()
