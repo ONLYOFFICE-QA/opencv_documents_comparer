529c3d3765d559228d0134aa41fe28323214ff46
kolamb <isrLamb@outlook.com>
2022-08-22 18:49:03 +0300

    Added send message to telegram

--- tasks.py
+++ tasks.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 import os
 
 from config import *
+from framework.telegram import Telegram
 from libs.functional.documents.doc_to_docx_image_compare import DocDocxCompareImg
 from libs.functional.documents.doc_to_docx_statistic_compare import DocDocxStatisticsCompare
 from libs.functional.documents.rtf_to_docx_image_compare import RtfDocxCompareImg
@@ -46,6 +47,7 @@ def run_doc_docx(c, full=False, st=False, ls=False, df=False, cl=False):
         else:
             img_compare = DocDocxCompareImg()
             img_compare.run_compare_word(os.listdir(img_compare.helper.converted_doc_folder))
+            Telegram.send_message('doc-docx comparison completed')
 
 
 @task(name="rtf-docx")
@@ -60,6 +62,7 @@ def run_rtf_docx(c, ls=False, cl=False):
             img_compare.run_compare_rtf_docx(list_of_files)
         else:
             img_compare.run_compare_rtf_docx(os.listdir(img_compare.word.helper.converted_doc_folder))
+            Telegram.send_message('rtf-docx comparison completed')
 
 
 @task(name="ppt-pptx")
@@ -73,6 +76,7 @@ def run_ppt_pptx(c, ls=False, cl=False):
         power_point.run_compare_pp(list_of_files)
     else:
         power_point.run_compare_pp(os.listdir(power_point.helper.converted_doc_folder))
+        Telegram.send_message('ppt-pptx comparison completed')
 
 
 @task(name="odp-pptx")
@@ -86,6 +90,7 @@ def run_odp_pptx(c, ls=False, cl=False):
         comparer.run_compare_odp_pptx(list_of_files)
     else:
         comparer.run_compare_odp_pptx(os.listdir(comparer.helper.converted_doc_folder))
+        Telegram.send_message('odp-pptx comparison completed')
 
 
 @task(name="xls_xlsx")
@@ -110,6 +115,7 @@ def run_xls_xlsx(c, full=False, st=False, ls=False, cl=False):
         else:
             excel = ExcelCompareImage()
             excel.run_compare_excel_img(os.listdir(excel.helper.converted_doc_folder))
+            Telegram.send_message('xls-xlsx comparison completed')
 
 
 @task
@@ -118,15 +124,18 @@ def opener_pptx(c, odp=False, ppt=False, ls=False):
         opener = OpenerPptx('odp')
         files_array = list_of_file_names if ls else os.listdir(opener.helper.converted_doc_folder)
         opener.run_opener(files_array)
+        Telegram.send_message('Odp => Pptx opening check completed')
     elif ppt:
         opener = OpenerPptx('ppt')
         files_array = list_of_file_names if ls else os.listdir(opener.helper.converted_doc_folder)
         opener.run_opener(files_array)
+        Telegram.send_message('Ppt => Pptx opening check completed')
     else:
         opener = OpenerPptx('ppt')
         opener.run_opener(os.listdir(opener.helper.converted_doc_folder))
         opener = OpenerPptx('odp')
         opener.run_opener(os.listdir(opener.helper.converted_doc_folder))
+        Telegram.send_message('Ppt => Pptx and Odp => Pptx opening check completed')
 
 
 @task
@@ -135,15 +144,18 @@ def opener_docx(c, doc=False, rtf=False, ls=False):
         opener = OpenerDocx('doc')
         files_array = list_of_file_names if ls else os.listdir(opener.helper.converted_doc_folder)
         opener.run_opener_word(files_array)
+        Telegram.send_message('Doc => Docx opening check completed')
     elif rtf:
         opener = OpenerDocx('rtf')
         files_array = list_of_file_names if ls else os.listdir(opener.helper.converted_doc_folder)
         opener.run_opener_word(files_array)
+        Telegram.send_message('Rtf => Docx opening check completed')
     else:
         opener = OpenerDocx('doc')
         opener.run_opener_word(os.listdir(opener.helper.converted_doc_folder))
         opener = OpenerDocx('rtf')
         opener.run_opener_word(os.listdir(opener.helper.converted_doc_folder))
+        Telegram.send_message('Doc => Docx and Rtf => Docx opening check completed')
 
 
 @task
@@ -152,23 +164,26 @@ def opener_xlsx(c, xls=False, ods=False, ls=False):
         opener = OpenerXlsx('xls')
         files_array = list_of_file_names if ls else os.listdir(opener.helper.converted_doc_folder)
         opener.run_opener_xlsx(files_array)
+        Telegram.send_message('Xls => Xlsx opening check completed')
 
     elif ods:
         opener = OpenerXlsx('ods')
         files_array = list_of_file_names if ls else os.listdir(opener.helper.converted_doc_folder)
         opener.run_opener_xlsx(files_array)
+        Telegram.send_message('Ods => Xlsx opening check completed')
     else:
         opener = OpenerXlsx('xls')
         opener.run_opener_xlsx(os.listdir(opener.helper.converted_doc_folder))
         opener = OpenerXlsx('ods')
         opener.run_opener_xlsx(os.listdir(opener.helper.converted_doc_folder))
-
+        Telegram.send_message('Ods => Xlsx and Xls => Xlsx opening check completed')
 
 @task
 def opener_odp(c, ls=False):
     opener = OpenerOdp('pptx')
     files_array = list_of_file_names if ls else os.listdir(opener.helper.converted_doc_folder)
     opener.run_opener_odp(files_array)
+    Telegram.send_message('Odp opening check completed')
 
 
 @task
@@ -176,6 +191,7 @@ def opener_odt(c, ls=False):
     opener = OpenerOdt('docx')
     files_array = list_of_file_names if ls else os.listdir(opener.helper.converted_doc_folder)
     opener.run_opener_odp(files_array)
+    Telegram.send_message('Odt opening check completed')
 
 
 @task
@@ -183,6 +199,7 @@ def opener_ods(c, ls=False):
     opener = OpenerOds('xlsx')
     files_array = list_of_file_names if ls else os.listdir(opener.helper.converted_doc_folder)
     opener.run_opener_ods(files_array)
+    Telegram.send_message('Ods opening check completed')
 
 
 @task(name="compare")
