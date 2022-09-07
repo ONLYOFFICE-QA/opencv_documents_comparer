aa6026c01113e4e479f3456aa5ca70457970a85e
kolamb <isrLamb@outlook.com>
2022-09-07 16:50:04 +0300

    fix Invalid window handle for MS Excel

--- framework/excel.py
+++ framework/excel.py
@@ -14,6 +14,7 @@ from loguru import logger
 from win32com.client import Dispatch
 
 from config import wait_for_opening
+from framework.telegram import Telegram
 from libs.helpers.compare_image import CompareImage
 from libs.helpers.error_handler import CheckErrors
 
@@ -23,37 +24,25 @@ class Excel:
     def __init__(self, helper):
         self.helper = helper
         self.check_errors = CheckErrors()
-        self.coordinate = []
-        self.statistics_excel = None
         self.click = self.helper.click
-        self.waiting_time = False
-
-    # gets the coordinates of the window
-    # sets the size and position of the window
-    def get_coord_exel(self, hwnd, ctx):
-        if win32gui.IsWindowVisible(hwnd):
-            if win32gui.GetClassName(hwnd) == 'XLMAIN':
-                win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)
-                win32gui.SetForegroundWindow(hwnd)
-                self.coordinate.clear()
-                self.coordinate.append(win32gui.GetWindowRect(hwnd))
+        self.statistics_excel = None
+        self.windows_handler_number = None
 
     def check_error(self, hwnd, ctx):
         if win32gui.IsWindowVisible(hwnd):
-            if win32gui.GetClassName(hwnd) == '#32770' \
-                    and win32gui.GetWindowText(hwnd) == "Microsoft Excel" \
-                    or win32gui.GetClassName(hwnd) == 'NUIDialog' \
-                    and win32gui.GetWindowText(hwnd) == "Microsoft Excel":
+            class_name = win32gui.GetClassName(hwnd)
+            window_text = win32gui.GetWindowText(hwnd)
+            if class_name == '#32770' and window_text == "Microsoft Excel" \
+                    or class_name == 'NUIDialog' and window_text == "Microsoft Excel":
                 win32gui.SetForegroundWindow(hwnd)
                 self.check_errors.errors.clear()
-                self.check_errors.errors.append(win32gui.GetClassName(hwnd))
-                self.check_errors.errors.append(win32gui.GetWindowText(hwnd))
+                self.check_errors.errors.append(class_name)
+                self.check_errors.errors.append(window_text)
 
-    def prepare_excel_windows(self):
+    def prepare_excel_for_test(self):
         try:
             pg.click('libs/image_templates/excel/turn_on_content.png')
             sleep(1)
-
             win32gui.EnumWindows(self.check_errors.get_windows_title, self.check_errors.errors)
             if self.check_errors.errors:
                 self.check_errors.errors.clear()
@@ -62,9 +51,6 @@ class Excel:
                 error_processing.start()
                 sleep(7)
                 error_processing.terminate()
-                sleep(0.5)
-                win32gui.EnumWindows(self.get_coord_exel, self.coordinate)
-
         except Exception:
             pass
 
@@ -76,22 +62,20 @@ class Excel:
     def check_open_excel(self, hwnd, ctx):
         if win32gui.IsWindowVisible(hwnd):
             if win32gui.GetClassName(hwnd) == 'XLMAIN' and win32gui.GetWindowText(hwnd) != '':
-                self.waiting_time = True
+                self.windows_handler_number = hwnd
 
     def waiting_for_opening_excel(self):
-        self.waiting_time = False
+        self.windows_handler_number = None
         stop_waiting = 1
         while True:
-            win32gui.EnumWindows(self.check_open_excel, self.waiting_time)
-            if self.waiting_time:
+            win32gui.EnumWindows(self.check_open_excel, self.windows_handler_number)
+            if self.windows_handler_number:
                 sleep(wait_for_opening)
                 break
             sleep(0.5)
             stop_waiting += 1
             if stop_waiting == 1000:
-                logger.error(f"'Too long to open "
-                             f"Copied files: {self.helper.converted_file} "
-                             f"and {self.helper.source_file} to 'failed_to_open_converted_file/too_long_to_open_files'")
+                logger.error(f"'Too long to open file: {self.helper.converted_file})")
                 self.helper.copy_to_folder(self.helper.too_long_to_open_files)
                 break
 
@@ -100,22 +84,15 @@ class Excel:
         if self.check_errors.errors \
                 and self.check_errors.errors[0] == "#32770" \
                 and self.check_errors.errors[1] == "Microsoft Excel":
-            logger.error(f"'an error has occurred while opening the file'. "
-                         f"Copied files: {self.helper.converted_file} "
-                         f"and {self.helper.source_file} to 'untested'")
-
+            logger.error(f"'an error has occurred while opening' when opening file: {self.helper.converted_file}.")
             pg.press('enter', presses=5)
-            self.helper.create_dir(self.helper.opener_errors)
             self.helper.copy_to_folder(self.helper.opener_errors)
             self.check_errors.errors.clear()
             return False
-
         elif not self.check_errors.errors:
             return True
-
         else:
-            logger.debug(f"Error message: {self.check_errors.errors} "
-                         f"Filename: {self.helper.converted_file}")
+            logger.debug(f"New Error\nError message: {self.check_errors.errors}\nFile: {self.helper.converted_file}")
             self.check_errors.errors.clear()
             return False
 
@@ -124,7 +101,6 @@ class Excel:
         if self.check_errors.errors \
                 and self.check_errors.errors[0] == 'NUIDialog' \
                 and self.check_errors.errors[1] == "Microsoft Excel":
-            pg.press('alt')
             pg.press('right')
             pg.press('enter')
             self.check_errors.errors.clear()
@@ -134,48 +110,53 @@ class Excel:
         if self.check_errors.errors \
                 and self.check_errors.errors[0] == "#32770" \
                 and self.check_errors.errors[1] == "Microsoft Excel":
-            pg.press('alt')
             pg.press('enter')
             self.check_errors.errors.clear()
 
-    # opens the document
-    # takes a screenshot by coordinates
-    def get_screenshots(self, path_to_save_screen):
-        self.prepare_excel_windows()
-        list_num = 1
-        win32gui.EnumWindows(self.get_coord_exel, self.coordinate)
-        coordinate = self.coordinate[0]
+    def set_windows_size_excel(self):
+        win32gui.ShowWindow(self.windows_handler_number, win32con.SW_MAXIMIZE)
+        win32gui.SetForegroundWindow(self.windows_handler_number)
+
+    # gets the coordinates of the window
+    # sets the size and position of the window
+    def get_coordinate_exel(self):
+        coordinate = [win32gui.GetWindowRect(self.windows_handler_number)]
+        coordinate = coordinate[0]
         coordinate = (coordinate[0] + 10,
                       coordinate[1] + 170,
                       coordinate[2] - 30,
                       coordinate[3] - 70)
+        return coordinate
 
-        for press in range(int(self.statistics_excel['num_of_sheets'])):
-            pg.hotkey('ctrl', 'pgup', interval=0.05)
-        for sheet in range(int(self.statistics_excel['num_of_sheets'])):
-            pg.hotkey('ctrl', 'home', interval=0.2)
-            page_num = 1
-            CompareImage.grab_coordinate_exel(path_to_save_screen,
-                                              list_num,
-                                              page_num,
-                                              coordinate)
-
-            if f'{list_num}_nrows' in self.statistics_excel:
-                num_of_row = self.statistics_excel[f'{list_num}_nrows'] / 65
-            else:
-                num_of_row = 2
-
-            for pgdwn in range(math.ceil(num_of_row)):
-                pg.press('pgdn', interval=0.5)
-                page_num += 1
-                CompareImage.grab_coordinate_exel(path_to_save_screen,
-                                                  list_num,
-                                                  page_num,
-                                                  coordinate)
-
-            pg.hotkey('ctrl', 'pgdn', interval=0.05)
-            sleep(0.5)
-            list_num += 1
+    # opens the document
+    # takes a screenshot by coordinates
+    def get_screenshots(self, path_to_save_screen):
+        if win32gui.IsWindow(self.windows_handler_number):
+            self.set_windows_size_excel()
+            coordinate = self.get_coordinate_exel()
+            self.prepare_excel_for_test()
+            list_num = 1
+            for press in range(int(self.statistics_excel['num_of_sheets'])):
+                pg.hotkey('ctrl', 'pgup', interval=0.05)
+            for sheet in range(int(self.statistics_excel['num_of_sheets'])):
+                pg.hotkey('ctrl', 'home', interval=0.2)
+                page_num = 1
+                CompareImage.grab_coordinate_exel(path_to_save_screen, list_num, page_num, coordinate)
+                if f'{list_num}_nrows' in self.statistics_excel:
+                    num_of_row = self.statistics_excel[f'{list_num}_nrows'] / 65
+                else:
+                    num_of_row = 2
+                for pgdwn in range(math.ceil(num_of_row)):
+                    pg.press('pgdn', interval=0.5)
+                    page_num += 1
+                    CompareImage.grab_coordinate_exel(path_to_save_screen, list_num, page_num, coordinate)
+                pg.hotkey('ctrl', 'pgdn', interval=0.05)
+                sleep(0.5)
+                list_num += 1
+        else:
+            message = f'Invalid window handle when get_screenshot, file: {self.helper.converted_file}'
+            Telegram.send_message(message)
+            logger.error(message)
 
     def get_excel_statistic(self, wb):
         self.statistics_excel = {
@@ -193,9 +174,11 @@ class Excel:
                 self.statistics_excel[f'{num_of_sheet}_ncols'] = ncols
                 num_of_sheet += 1
 
-        except Exception:
-            logger.error(f'\nFailed to get full statistics excel from file: {self.helper.converted_file}\n '
-                         f'statistics: {self.statistics_excel}')
+        except Exception as e:
+            massage = f'Failed to get full statistics excel from file: {self.helper.converted_file}\n '\
+                      f'statistics: {self.statistics_excel}\nException: {e}'
+            logger.error(massage)
+            Telegram.send_message(massage)
 
     def opener_excel(self, file_name):
         error_processing = Process(target=self.check_errors.run_get_error_exel, args=(self.helper.converted_file,))
@@ -209,11 +192,10 @@ class Excel:
             print(f"[bold blue]Number of sheets[/]: {self.statistics_excel['num_of_sheets']}")
             return True
 
-        except Exception:
+        except Exception as e:
             error = traceback.format_exc()
-            logger.error(f'{error} happened while opening file: {self.helper.converted_file}')
+            logger.error(f'{error} happened while opening file: {self.helper.converted_file} \nException: {e}')
             self.statistics_excel = None
-            logger.error(f"Can't open file: {self.helper.source_file}. Copied files to 'untested'")
             self.helper.copy_to_folder(self.helper.failed_source)
             return False
 
@@ -230,8 +212,8 @@ class Excel:
         try:
             workbooks.Close(False)
             excel.Quit()
-        except Exception:
+        except Exception as e:
             error = traceback.format_exc()
-            logger.error(f'{error} happened while closing file: {self.helper.converted_file}')
+            logger.error(f'{error} happened while closing file: {self.helper.converted_file}\nException: {e}')
         finally:
             os.system("taskkill /t /im  EXCEL.EXE")
