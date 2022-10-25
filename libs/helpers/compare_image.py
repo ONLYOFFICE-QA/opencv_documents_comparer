ad2e2d779d4eedfb187fb43be2fe82efce1d3a2b
kolamb <isrLamb@outlook.com>
2022-10-25 18:09:05 +0300

    Refactored Compare image module

--- libs/helpers/compare_image.py
+++ libs/helpers/compare_image.py
@@ -1,172 +1,108 @@
 # -*- coding: utf-8 -*-
+import os
 import re
-
+from loguru import logger
 import cv2
 import imageio
 import numpy as np
 from PIL import ImageGrab
 from rich.progress import track
+from rich import print
 from skimage.metrics import structural_similarity
 
-from libs.helpers.helper import *
+from data.StaticData import StaticData
+from framework.fileutils import FileUtils
 
 
 class CompareImage:
+    def __init__(self, coefficient=98):
+        self.coefficient = coefficient
+        self.helper = StaticData.DOC_ACTIONS
+        self.similarity: int = 0
+        self.image_name, self.sheet_num, self.gif_name = '', '', ''
+        self.source_img, self.converted_img, self.src_processed_img, self.cnv_processed_img = [], [], [], []
+        self.result_folder_name = self.get_result_folder_name()
+        self.img_comparison_diff_dir = f'{self.helper.result_folder}/img_comparison_diff/{self.result_folder_name}/'
+        self.passed = f'{self.helper.result_folder}/passed/'
+        self.start_compare_images()
 
-    def __init__(self, helper, koff=98, odp=False):
-        self.odp = odp
-        self.koff = koff
-        self.helper = helper
-        self.screen_folder = 'screen'
-        self.folder_name = self.helper.converted_file.replace(f'.{self.helper.converted_file.split(".")[-1]}', '')
-        self.folder_name = re.sub(r"^\s+|\n|\r|\.|,|-|\s|\s+$", '', self.folder_name)
-        self.folder_name = self.folder_name[:35]
-        Helper.create_dir(f'{self.helper.passed}{self.folder_name}')
-        Helper.create_dir(f'{self.helper.passed}{self.folder_name}/{self.screen_folder}')
-        self.start_to_compare_images()
+    def get_result_folder_name(self):
+        result_folder_name = self.helper.converted_file.replace(f'.{self.helper.converted_extension}', '')
+        result_folder_name = re.sub(r"^\s+|\n|\r|\.|,|-|\s|\s+$", '', result_folder_name)
+        result_folder_name = result_folder_name[:35]
+        return result_folder_name
 
-    def start_to_compare_images(self):
-        for image_name in track(os.listdir(self.helper.tmp_dir_converted_image),
-                                description='[bold blue]Comparing In Progress[/bold blue]'):
-
-            if os.path.exists(f'{self.helper.tmp_dir_source_image}{image_name}') \
-                    and os.path.exists(f'{self.helper.tmp_dir_converted_image}{image_name}'):
-
-                self.compare_img(f'{self.helper.tmp_dir_source_image}{image_name}',
-                                 f'{self.helper.tmp_dir_converted_image}{image_name}',
-                                 image_name,
-                                 )
-
+    def start_compare_images(self):
+        description = "[bold blue]Comparing In Progress[/]"
+        for self.image_name in track(os.listdir(StaticData.TMP_DIR_CONVERTED_IMG), description=description):
+            if os.path.exists(f'{StaticData.TMP_DIR_SOURCE_IMG}/{self.image_name}') \
+                    and os.path.exists(f'{StaticData.TMP_DIR_CONVERTED_IMG}/{self.image_name}'):
+                self.sheet_num = self.image_name.split('_')[-1].replace('.png', '')
+                self.gif_name = f"{self.image_name.split('.')[0]}_similarity.gif"
+                self.source_img = cv2.imread(f'{StaticData.TMP_DIR_SOURCE_IMG}{self.image_name}')
+                self.converted_img = cv2.imread(f'{StaticData.TMP_DIR_CONVERTED_IMG}{self.image_name}')
+                self.image_handler()
+                print(f"[blue]{self.helper.converted_file} Sheet: {self.sheet_num} similarity[/]: {self.similarity}")
+                self.save_result() if self.similarity < self.coefficient else print('[bold green]passed')
             else:
-                logger.error(f'Image {image_name} not found, '
-                             f'copied file {self.helper.converted_file} '
-                             f'and {self.helper.source_file} to "Untested"')
+                logger.error(f'Image {self.image_name} not found, copied file to "Untested"')
+                self.helper.copy_testing_files_to_folder(self.helper.untested_folder)
 
-                self.helper.copy_to_folder(self.helper.untested_folder)
+    def put_information_on_img(self):
+        self.put_text(self.converted_img, f'After')
+        self.put_text(self.source_img, f'Before')
+        self.put_text(self.src_processed_img, f'Before sheet: {self.sheet_num}. Similarity{self.similarity}%')
+        self.put_text(self.cnv_processed_img, f'After sheet: {self.sheet_num}. Similarity{self.similarity}%')
 
-        self.helper.delete(f'{self.helper.tmp_dir_converted_image}*')
-        self.helper.delete(f'{self.helper.tmp_dir_source_image}*')
-
-    def compare_img(self, image_before_conversion, image_after_conversion, image_name):
-        image_name = image_name.split('.')[0]
-        sheet = image_name.split('_')[-1]
-        file_name_for_gif = f'{image_name}_similarity.gif'
-        before_full = cv2.imread(image_before_conversion)
-        after_full = cv2.imread(image_after_conversion)
-
-        if self.helper.converted_file.endswith((".xlsx", ".XLSX")):
-            after, before, similarity = self.find_difference(after_full, before_full)
-            pass
+    def image_handler(self):
+        if self.helper.converted_file.lower().endswith(".xlsx"):
+            self.find_difference(self.source_img, self.converted_img)
         else:
-            if self.odp:
-                before = self.find_contours(image_before_conversion, odp=True)
-            else:
-                before = self.find_contours(image_before_conversion)
-            after = self.find_contours(image_after_conversion)
-
             try:
-                after, before, similarity = self.find_difference(after, before)
-            except Exception:
-                after, before, similarity = self.find_difference(after_full, before_full)
-                pass
+                self.find_difference(self.find_contours(self.source_img), self.find_contours(self.converted_img))
+            except ValueError:
+                self.find_difference(self.source_img, self.converted_img)
+        self.put_information_on_img()
 
-        print(f"[bold blue]{self.helper.converted_file}[/bold blue] Sheet: {sheet} "
-              f"[bold blue]similarity[/bold blue]: {similarity}")
+    def save_result(self):
+        self.helper.copy_testing_files_to_folder(self.img_comparison_diff_dir)
+        FileUtils.create_dir(f'{self.img_comparison_diff_dir}/gif/')
+        FileUtils.create_dir(f'{self.img_comparison_diff_dir}/screen/')
+        collage = np.hstack([self.source_img, self.converted_img])
+        cv2.imwrite(f'{self.img_comparison_diff_dir}/screen/{self.image_name}_collage.png', collage)
+        images = [self.src_processed_img, self.cnv_processed_img]
+        imageio.mimsave(f'{self.img_comparison_diff_dir}/gif/{self.gif_name}', images, duration=1)
 
-        before = self.put_text(before, f'Before sheet {sheet}. Similarity {round(similarity, 3)}%')
-        after = self.put_text(after, f'After sheet {sheet}. Similarity {round(similarity, 3)}%')
-        collage = self.collage(after_full, before_full)
-        images = [before, after]
-        if similarity < self.koff:
-            self.write_down_results(collage, images, file_name_for_gif, image_name)
-        else:
-            print('[bold green]passed[/bold green]')
-            imageio.mimsave(f'{self.helper.passed}{self.folder_name}/{file_name_for_gif}',
-                            images,
-                            duration=1)
-            cv2.imwrite(
-                f'{self.helper.passed}{self.folder_name}/{self.screen_folder}/{image_name}_collage.png',
-                collage)
-
-    def collage(self, after_img, before_img):
-        after_for_collage = self.put_text(after_img, f'After')
-        before_for_collage = self.put_text(before_img, f'Before')
-        collage = np.hstack([before_for_collage, after_for_collage])
-        return collage
-
-    def write_down_results(self, collage, images, file_name_for_gif, image_name):
-        self.helper.create_dir(f'{self.helper.differences_compare_image}{self.folder_name}')
-        self.helper.create_dir(f'{self.helper.differences_compare_image}{self.folder_name}/gif/')
-        self.helper.create_dir(f'{self.helper.differences_compare_image}{self.folder_name}/{self.screen_folder}')
-        self.helper.copy_to_folder(f'{self.helper.differences_compare_image}{self.folder_name}/')
-        cv2.imwrite(
-            f'{self.helper.differences_compare_image}{self.folder_name}/{self.screen_folder}/'
-            f'{image_name}_collage.png',
-            collage)
-        imageio.mimsave(f'{self.helper.differences_compare_image}{self.folder_name}/gif/{file_name_for_gif}',
-                        images,
-                        duration=1)
-        pass
-
-    @staticmethod
-    def find_contours(img, odp=False):
-        img_source = cv2.imread(img)
-        img = cv2.cvtColor(img_source, cv2.COLOR_BGR2RGB)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    def find_contours(self, img):
+        rgb, gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB), cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         _, binary = cv2.threshold(gray, 125, 255, cv2.THRESH_BINARY)
         contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        for c in contours:
-            x, y, w, h = cv2.boundingRect(c)
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
             if h >= 500:
-                # to save the images
-                if odp:
-                    img = img[y:y + h + 1, x:x + w + 1]
-                else:
-                    img = img[y:y + h, x:x + w]
-                return img
+                return rgb[y:y + h + 1, x:x + w + 1] if self.helper.source_extension == 'odp' else rgb[y:y + h, x:x + w]
 
-    @staticmethod
-    def find_difference(after, before):
-        before_gray = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
-        after_gray = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
+    def find_difference(self, src_img, cnv_img):
+        before_gray, after_gray = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY), cv2.cvtColor(cnv_img, cv2.COLOR_BGR2GRAY)
         (score, diff) = structural_similarity(before_gray, after_gray, full=True)
         diff = (diff * 255).astype("uint8")
         thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
         contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours = contours[0] if len(contours) == 2 else contours[1]
-        color = (0, 0, 255)
-        for c in contours:
-            area = cv2.contourArea(c)
+        for contur in contours:
+            area = cv2.contourArea(contur)
             if area > 40:
-                x, y, w, h = cv2.boundingRect(c)
-                cv2.rectangle(before, (x, y), (x + w, y + h), color, 0)
-                cv2.rectangle(after, (x, y), (x + w, y + h), color, 0)
-        similarity = score * 100
-        return after, before, similarity
+                x, y, w, h = cv2.boundingRect(contur)
+                cv2.rectangle(src_img, (x, y), (x + w, y + h), (0, 0, 255), 0)
+                cv2.rectangle(cnv_img, (x, y), (x + w, y + h), (0, 0, 255), 0)
+        self.similarity = round(score * 100, 3)
+        self.src_processed_img, self.cnv_processed_img = src_img, cnv_img
 
     @staticmethod
-    def put_text(image, text):
-        color = (0, 0, 255)
-        cv2.putText(image, f'{text}',
-                    (20, 35),
-                    cv2.FONT_HERSHEY_COMPLEX,
-                    1,
-                    color=color,
-                    thickness=2)
-        return image
+    def put_text(cv2_opened_image, text):
+        cv2.putText(cv2_opened_image, text, (20, 35), cv2.FONT_HERSHEY_COMPLEX, 1, color=(0, 0, 255), thickness=2)
 
     @staticmethod
-    def grab_coordinate_exel(path, list_num, number_of_pages, coordinate):
+    def grab_coordinate(path_to_img_for_save, coordinate):
         image = ImageGrab.grab(bbox=coordinate)
-        image.save(path + str(f'_list_{list_num}_page_{number_of_pages}') + '.png', 'PNG')
-
-    @staticmethod
-    def grab_coordinate(path, number_of_pages, coordinate):
-        image = ImageGrab.grab(bbox=coordinate)
-        image.save(path + str(f'page_{number_of_pages}') + '.png', 'PNG')
-
-    @staticmethod
-    def grab(path, filename, number_of_pages):
-        filename_for_screen = filename.replace(f'.{filename.split(".")[-1]}', '')
-        im = ImageGrab.grab()
-        im.save(path + filename_for_screen + str(f'_{number_of_pages}') + '.png', 'PNG')
+        image.save(path_to_img_for_save, 'PNG')
