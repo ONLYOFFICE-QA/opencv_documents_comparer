994dc577051b62325fca8bf852537f9ad943ecef
l02 <l8556@outlook.com>
2022-12-23 20:31:17 +0300

    Added make requirements.txt from poetry.lock file

---
+++ make_requirements.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import subprocess as sb
+
+sb.call('pip install tomlkit==0.11.6', shell=True)
+
+with open("requirements.txt", 'w') as file:
+    file.write('# -*- coding: utf-8 -*-\n')
+with open("poetry.lock") as t:
+    import tomlkit
+    lock = tomlkit.parse(t.read())
+    for package in lock['package']:
+        with open("requirements.txt", 'a') as file:
+            file.write(f"{package['name']}=={package['version']}\n")
+sb.call('pip uninstall tomlkit -y', shell=True)
