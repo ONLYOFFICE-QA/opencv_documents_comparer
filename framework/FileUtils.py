c19501954d5bd047b6eff290b06e05ad5aeced6e
l02 <l8556@outlook.com>
2023-01-28 15:55:45 +0300

    Added method to get file paths including subdirectories

--- framework/FileUtils.py
+++ framework/FileUtils.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import os
+import codecs
 import json
 import random
 import shutil
 import subprocess as sb
-import codecs
 import zipfile
+from os import listdir, makedirs, scandir, remove, walk
+from os.path import exists, isfile, isdir, join, getctime
 
 import py7zr
 from rich import print
@@ -18,45 +19,66 @@ class FileUtils:
         with codecs.open(path_to_json, "r", "utf_8_sig") as file:
             return json.load(file)
 
+    @staticmethod
+    def delete_last_slash(path):
+        return path.rstrip(path[-1]) if path[-1] in ['/', '\\'] else path
+
+    @staticmethod
+    def get_file_paths(dir_path):
+        files_paths = []
+        for root, dirs, files in walk(dir_path):
+            for filename in files:
+                files_paths.append(join(root, filename))
+        return files_paths
+
+    @staticmethod
+    def get_files_by_extensions(path_to_folder, ext):
+        files_paths = []
+        for root, dirs, files in walk(path_to_folder):
+            for filename in files:
+                if ext and filename.lower().endswith(ext if isinstance(ext, tuple) else ext.lower()):
+                    files_paths.append(join(root, filename))
+        return files_paths
+
     # path insert with file name
     @staticmethod
     def copy(path_from, path_to, silence=False):
-        if not os.path.exists(path_from):
+        if not exists(path_from):
             return print(f"[red]copy warning: path not exist: {path_from}")
-        if os.path.isdir(path_from):
+        if isdir(path_from):
             shutil.copytree(path_from, path_to)
-        elif os.path.isfile(path_from):
+        elif isfile(path_from):
             shutil.copyfile(path_from, path_to)
-        if os.path.exists(path_to):
-            return print(f'[green]Copied To:[/] {path_to}') if not silence else None
-        return print(f'[red]copy warning, File not copied: {path_to}[/]')
+        if exists(path_to):
+            return print(f'[green]Copied To: {path_to}') if not silence else None
+        return print(f'[red]copy warning, File not copied: {path_to}')
 
     @staticmethod
     def last_modified_file(dir_path):
-        files = [file for file in os.listdir(dir_path) if file != '.DS_Store']
-        files = [os.path.join(dir_path, file) for file in files if os.path.isfile(os.path.join(dir_path, file))]
-        return max(files, key=os.path.getctime) if files else print('[bold red]last modified file not found')
+        files = [file for file in listdir(dir_path) if file != '.DS_Store']
+        files = [join(dir_path, file) for file in files if isfile(join(dir_path, file))]
+        return max(files, key=getctime) if files else print('[bold red]last modified file not found')
 
     @staticmethod
     def move(path_from, path_to):
-        if os.path.exists(path_from):
+        if exists(path_from):
             shutil.move(path_from, path_to)
-            return print("[bold red]Move warning. File not moved") if not os.path.exists(path_to) else None
+            return print("[bold red]Move warning. File not moved") if not exists(path_to) else None
         return print(f"[bold red]Move warning. File not exist: {path_from}")
 
     @staticmethod
     def create_dir(path_to_dir, silence=False):
-        if not os.path.exists(path_to_dir):
-            os.makedirs(path_to_dir)
-            if os.path.isdir(path_to_dir):
-                return print(f'[green]Folder Created:[/] {path_to_dir}') if not silence else None
+        if not exists(path_to_dir):
+            makedirs(path_to_dir)
+            if isdir(path_to_dir):
+                return print(f'[green]Folder Created: {path_to_dir}') if not silence else None
             return print(f'[bold red]Create folder warning. Folder not created: {path_to_dir}')
 
     @staticmethod
     def unpacking_via_7zip(archive_path, execute_path, delete=False):
         with py7zr.SevenZipFile(archive_path, 'r') as archive:
             archive.extractall(path=execute_path)
-            print(f'[green]Unpack Completed.[/]')
+            print(f'[green]Unpack Completed.')
         FileUtils.delete(archive_path, silence=True) if delete else ''
 
     @staticmethod
@@ -68,30 +90,28 @@ class FileUtils:
 
     @staticmethod
     def delete(what_delete, all_from_folder=False, silence=False):
-        if not os.path.exists(what_delete):
+        if not exists(what_delete):
             return print(f"[red]Delete warning. path not exist: {what_delete}") if not silence else None
-        if os.path.isdir(what_delete):
+        if isdir(what_delete):
             shutil.rmtree(what_delete, ignore_errors=True)
             if all_from_folder:
                 FileUtils.create_dir(what_delete)
-                if any(os.scandir(what_delete)):
+                if any(scandir(what_delete)):
                     return print(f"[red]Delete warning. Error while delete all from folder: {what_delete}")
                 return print(f'[green]Folder is cleared') if not silence else None
-        elif os.path.isfile(what_delete):
-            os.remove(what_delete)
-        if os.path.exists(what_delete):
+        elif isfile(what_delete):
+            remove(what_delete)
+        if exists(what_delete):
             return print(f"[red]Delete warning. Folder exist: {what_delete}")
-        print(f'[green]Object Deleted:[/] {what_delete}') if not silence else None
+        print(f'[green]Deleted: {what_delete}') if not silence else None
 
     @staticmethod
-    def random_name(path, file_extension=None):
+    def random_name(path, extension=None):
         while True:
-            if file_extension:
-                random_file_name = f'{random.randint(5000, 50000000)}.{file_extension}'
-            else:
-                random_file_name = f'{random.randint(5000, 50000000)}'
-            if not os.path.exists(f'{path}/{random_file_name}'):
-                return os.path.join(path, random_file_name)
+            random_name = f'{random.randint(500, 50000)}.{extension}' if extension else f'{random.randint(500, 50000)}'
+            random_object_path = join(path, random_name)
+            if not exists(random_object_path):
+                return random_object_path
 
     @staticmethod
     def run_command(command):
