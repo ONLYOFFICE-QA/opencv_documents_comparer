324945f66fbda25e69142f49ce3acf4fb3c9813e
l02 <l8556@outlook.com>
2023-02-02 14:16:35 +0300

    Minor refactoring module converter

--- framework/converter.py
+++ framework/converter.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import json
-import time
+import subprocess as sb
+from os import chdir
+from os.path import join
 
 from rich import print
+from rich.prompt import Prompt
 
 import settings
 from configurations.project_configurator import ProjectConfig
 from framework.FileUtils import FileUtils
 from framework.actions.document_actions import DocActions
+from framework.actions.host_actions import HostActions
 from framework.actions.x2t_actions import X2t
-from framework.actions.x2ttester_actions import X2ttester
 from framework.actions.xml_actions import XmlActions
 from framework.singleton import singleton
 from framework.xmllint import XmlLint
@@ -19,33 +22,35 @@ from framework.xmllint import XmlLint
 class Converter:
     def __init__(self):
         self.xml = XmlActions()
-        self.x2ttester = X2ttester()
+        self.host = HostActions()
         self.xmllint = XmlLint()
+        self.x2t_version = None
 
     @staticmethod
-    def getting_formats(direction):
-        if direction:
-            return direction.split('-')[0], direction.split('-')[1]
-        else:
-            return None, None
+    def getting_formats(direction=None):
+        _direction = direction if direction else Prompt.ask('Input formats with -', default=None, show_default=False)
+        if _direction:
+            if '-' in _direction:
+                return _direction.split('-')[0], _direction.split('-')[1]
+            return None, _direction
+        return None, None
 
     def conversion_via_x2ttester(self, input_format, output_format, ls=False):
-        start_time = time.time()
-        if ls:
-            list_xml = self.xml.generate_files_list()
-            self.x2ttester.run_x2ttester(input_format, output_format, list_xml)
-        else:
-            self.x2ttester.run_x2ttester(input_format, output_format)
-        print(f"[bold red]{X2t.x2t_version()}")
-        print(time.time() - start_time)
+        chdir(ProjectConfig.core_dir())
+        self.x2t_version = X2t.x2t_version()
+        print(f"[bold green]|INFO| The conversion is running on x2t version: [bold red]{self.x2t_version}")
+        path_to_list = self.xml.generate_files_list() if ls else None
+        tmp_xml = self.xml.generate_x2ttester_parameters(input_format, output_format, path_to_list, self.x2t_version)
+        sb.call(f"{join(ProjectConfig.core_dir(), self.host.x2ttester)} {tmp_xml}", shell=True)
+        print(f"[bold red]|INFO| x2t version: {self.x2t_version}")
 
     def convert_from_extension_array(self):
         for extensions in json.load(open(f"{ProjectConfig.PROJECT_DIR}/data/extension_array.json")).items():
-            src_extension = extensions[0]
-            for cnv_extension in extensions[1]:
+            src_ext = extensions[0]
+            for cnv_ext in extensions[1]:
                 settings.delete = '0'
-                self.x2ttester.run_x2ttester(src_extension, cnv_extension)
-                result_folder = f"{ProjectConfig.result_dir()}/{X2t.x2t_version()}_{src_extension}_{cnv_extension}"
-                DocActions.copy_result_x2ttester(result_folder, cnv_extension)
+                self.conversion_via_x2ttester(src_ext, cnv_ext)
+                result_dir = join(ProjectConfig.result_dir(), f"{self.X2ttester.x2t_version}_{src_ext}_{cnv_ext}")
+                DocActions.copy_result_x2ttester(result_dir, cnv_ext)
                 self.xmllint.run_tests(ProjectConfig.tmp_result_dir())
                 FileUtils.delete(ProjectConfig.tmp_result_dir())
