c4a58054d119ea1acf277517763be682d63f6c51
l02 <l8556@outlook.com>
2023-02-02 14:15:59 +0300

    Minor refactoring module x2ttester actions

--- framework/actions/x2ttester_actions.py
+++ framework/actions/x2ttester_actions.py
@@ -14,11 +14,12 @@ class X2ttester:
     def __init__(self):
         self.host_config = HostActions()
         self.xml = XmlActions()
-        self.x2t_version = ''
+        self.x2t_version = None
 
     def run_x2ttester(self, input_format, output_format, path_to_list=None):
         self.x2t_version = X2t.x2t_version()
-        print(f"[bold green]The conversion is running on the x2t version:[/] [red]{self.x2t_version}")
+        print(f"[bold green]|INFO|The conversion is running on x2t version: [red]{self.x2t_version}")
         tmp_xml = self.xml.generate_x2ttester_parameters(input_format, output_format, path_to_list, self.x2t_version)
         chdir(ProjectConfig.core_dir())
         sb.call(f"{ProjectConfig.core_dir()}/{self.host_config.x2ttester} {tmp_xml}", shell=True)
+        print(f"[bold red]|WARNING|x2t version: {self.x2t_version}")
