5ec2dbfb5e0c37677c5b014db4f34e26048e648b
l02 <l8556@outlook.com>
2023-02-02 14:15:39 +0300

    Minor refactoring module x2t actions

--- framework/actions/x2t_actions.py
+++ framework/actions/x2t_actions.py
@@ -1,22 +1,18 @@
 # -*- coding: utf-8 -*-
-import subprocess as sb
 from os import chdir
 from os.path import exists
 from os.path import join
 
 from configurations.project_configurator import ProjectConfig
+from framework.FileUtils import FileUtils
 from framework.actions.host_actions import HostActions
 
 
 class X2t:
     @staticmethod
-    def x2t_version():
+    def x2t_version(x2t_dir=ProjectConfig.core_dir()):
         host = HostActions()
-        if exists(join(ProjectConfig.core_dir(), host.x2t)):
-            chdir(ProjectConfig.core_dir()) if host.os == 'mac' else None
-            for line in sb.getoutput(join(ProjectConfig.core_dir(), host.x2t)).split("\n"):
-                if ':' in line:
-                    key, value = line.strip().split(':', 1)
-                    if key.lower() == 'oox/binary file converter. version':
-                        return value.strip()
-        return ''
+        if exists(join(x2t_dir, host.x2t)):
+            chdir(x2t_dir)
+            x2t_info = FileUtils.output_cmd(join(ProjectConfig.core_dir(), host.x2t))
+            return FileUtils.find_in_line_by_key(x2t_info, 'OOX/binary file converter. Version')
