ac0bfb34a79bb24bb7a971be283812caa7c1ab34
l02 <l8556@outlook.com>
2023-07-20 14:39:31 +0300

    added telegram package

--- frameworks/telegram/telegram.py
+++ frameworks/telegram/telegram.py
@@ -1,49 +1,89 @@
 # -*- coding: utf-8 -*-
+import json
 import os
 from os.path import join, getsize, basename, isdir
 
 import requests
+from rich import print
 from loguru import logger
 
 from frameworks.StaticData import StaticData
-from frameworks.host_control.FileUtils import FileUtils
-from settings import version
+from frameworks.decorators import singleton
+from frameworks.host_control import FileUtils
 
 
+@singleton
 class Telegram:
-    telegram_token, chat_id = os.environ.get('TELEGRAM_TOKEN'), os.environ.get('CHANNEL_ID')
+    def __init__(self):
+        self._telegram_token = os.environ.get('TELEGRAM_TOKEN')
+        self._chat_id = os.environ.get('CHANNEL_ID')
+        self.tmp_dir = StaticData.tmp_dir
+        FileUtils.create_dir(self.tmp_dir)
 
-    @staticmethod
-    def send_message(message: str):
-        if Telegram.telegram_token and Telegram.chat_id:
+    def send_message(self, message: str, out_msg=False) -> None:
+        print(message) if out_msg else ...
+        if self._access:
             if len(message) > 4096:
-                return Telegram.send_long_message_as_document(message)
-            url = f"https://api.telegram.org/bot{Telegram.telegram_token}/sendMessage"
-            response = requests.post(url, data={"chat_id": Telegram.chat_id, "text": message, "parse_mode": "Markdown"})
+                document = self._make_massage_doc(message=message)
+                self.send_document(document, caption=self._prepare_caption(message))
+                return FileUtils.delete(document)
+            self._request(
+                f"https://api.telegram.org/bot{self._telegram_token}/sendMessage",
+                data={"chat_id": self._chat_id, "text": message, "parse_mode": "Markdown"},
+                tg_log=False
+            )
+
+    def send_document(self, document_path: str, caption: str = '') -> None:
+        if self._access:
+            self._request(
+                f"https://api.telegram.org/bot{self._telegram_token}/sendDocument",
+                data={"chat_id": self._chat_id, "caption": self._prepare_caption(caption), "parse_mode": "Markdown"},
+                files={"document": open(self._prepare_documents(document_path), 'rb')}
+            )
+
+    def send_media_group(self, document_paths: list, caption: str = None, media_type: str = 'document') -> None:
+        if self._access:
+            if caption and len(caption) > 200:
+                document_paths.append(self._make_massage_doc(caption, 'caption.txt'))
+            files, media = {}, []
+            for doc_path in document_paths:
+                files[basename(doc_path)] = open(self._prepare_documents(doc_path), 'rb')
+                media.append(dict(type=media_type, media=f'attach://{basename(doc_path)}'))
+            media[-1]['caption'] = self._prepare_caption(caption) if caption is not None else ''
+            self._request(
+                f'https://api.telegram.org/bot{self._telegram_token}/sendMediaGroup',
+                data={'chat_id': self._chat_id, 'media': json.dumps(media), "parse_mode": "Markdown"},
+                files=files
+            )
+
+    @staticmethod
+    def _prepare_caption(caption: str) -> str:
+        return caption[:200]
+
+    def _request(self, url: str, data: dict, files: dict = None, tg_log: bool = True) -> None:
+        try:
+            response = requests.post(url, data=data, files=files)
             if response.status_code != 200:
-                logger.error(f"post_text error text: {message}")
+                logger.error(f"Error when sending a document")
+                self.send_message(f"Error when sending to telegram: {response.status_code}") if tg_log else ...
+        except Exception as e:
+            logger.error(f"|WARNING|Impossible to send: {data}. Error: {e}")
+            self.send_message(f"|WARNING|Impossible to send: {data}. Error: {e}") if tg_log else ...
 
-    @staticmethod
-    def send_document(document_path, caption=''):
-        if Telegram.telegram_token and Telegram.chat_id:
-            if getsize(document_path) >= 50_000_000 or isdir(document_path):
-                FileUtils.compress_files(document_path, join(StaticData.TMP_DIR, f'{basename(document_path)}.zip'))
-                document_path = join(StaticData.TMP_DIR, f'{basename(document_path)}.zip')
-            try:
-                response = requests.post(f"https://api.telegram.org/bot{Telegram.telegram_token}/sendDocument",
-                                         data={"chat_id": Telegram.chat_id, "caption": caption,
-                                               "parse_mode": "Markdown"},
-                                         files={"document": open(document_path, 'rb')})
-                if response.status_code != 200:
-                    logger.error(f"Error when sending a document")
-            except Exception as e:
-                Telegram.send_message(f"|WARNING|Impossible to send: {document_path}. Error: {e}")
+    def _prepare_documents(self, doc_path: str) -> str:
+        if getsize(doc_path) >= 50_000_000 or isdir(doc_path):
+            FileUtils.compress_files(doc_path, join(self.tmp_dir, f'{basename(doc_path)}.zip'))
+            return join(self.tmp_dir, f'{basename(doc_path)}.zip')
+        return doc_path
 
-    @staticmethod
-    def send_long_message_as_document(long_message: str):
-        if Telegram.telegram_token and Telegram.chat_id:
-            report = join(StaticData.TMP_DIR, 'report.txt')
-            with open(report, "w") as file:
-                file.write(long_message)
-            Telegram.send_document(report, caption=f"report.txt\nVersion:{version}")
-            FileUtils.delete(report)
+    def _make_massage_doc(self, message: str, name: str = 'report.txt') -> str:
+        doc_path = join(self.tmp_dir, name)
+        with open(doc_path, "w") as file:
+            file.write(message)
+        return doc_path
+
+    @property
+    def _access(self) -> bool:
+        if self._telegram_token and self._chat_id:
+            return True
+        return False
