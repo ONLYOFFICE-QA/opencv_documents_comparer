16932f0d95b248eca3476793a091394328ecf9cb
l02 <l8556@outlook.com>
2023-07-20 14:32:42 +0300

    Refactoring

---
+++ config.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+version = ''
+
+# Path to source documents
+source_docs = r''
+# Path to converted documents
+converted_docs = r''
+
+# Delay time after opening a document in the editors in seconds
+delay_word = 2
+delay_excel = 2
+delay_power_point = 2
+delay_libre = 2
+
+# Maximum amount of time the document is waiting to be opened.
+max_waiting_time = 60
+
+# x2ttester settings
+cores = 4
+errors_only = True
+delete = False
+timestamp = True
+timeout = 3600
+# Path to MS Office
+ms_office = r'C:/Program Files/Microsoft Office/root/Office16'
+# Path to LibreOffice
+libre_office = r'C:/Program Files/LibreOffice/program'
+
+# To test from the names array
+files_array = []
