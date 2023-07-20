8ef93fdf914cc05c3005e7a5ef976e5481c3e28a
l02 <l8556@outlook.com>
2023-07-20 14:45:30 +0300

    added test run

--- tasks.py
+++ tasks.py
@@ -1,179 +1,89 @@
 # -*- coding: utf-8 -*-
-import platform
-from os.path import join
-
 from invoke import task
 from rich import print
 from rich.prompt import Prompt
 
-import settings
+import config
 from frameworks.StaticData import StaticData
-from frameworks.documents_actions import DocActions
-from frameworks.host_control import FileUtils
-from frameworks.onlyoffice import X2ttester, Core
-from frameworks.onlyoffice.x2ttester.x2ttester_report import X2ttesterReport
-from frameworks.telegram.telegram import Telegram
-from frameworks.xmllint import XmlLint
 
-if platform.system().lower() == 'windows':
-    from libs.comparison.presentation.odp_to_pptx_compare import OdpPptxCompare
-    from libs.comparison.presentation.ppt_to_pptx_compare import PptPptxCompareImg
-    from libs.comparison.spreadsheets.xls_to_xlsx_image_compare import ExcelCompareImage
-    from libs.comparison.spreadsheets.xls_to_xlsx_statistic_compare import StatisticCompare
-    from libs.comparison.documents.doc_to_docx_image_compare import DocDocxCompareImg
-    from libs.comparison.documents.doc_to_docx_statistic_compare import DocDocxStatisticsCompare
-    from libs.comparison.documents.rtf_to_docx_image_compare import RtfDocxCompareImg
-    from libs.openers.opener_docx_with_ms_word import OpenerDocx
-    from frameworks.libre_office import OpenerLibre
-    from libs.openers.opener_pptx_with_ms_power_point import OpenerPptx
-    from libs.openers.opener_xlsx_with_ms_excel import OpenerXlsx
+from frameworks.host_control import FileUtils, HostInfo
+from frameworks.editors.onlyoffice import Core, X2t
+from frameworks.telegram import Telegram
+from tests import X2tTesterConversion
+
+if HostInfo().os == 'windows':
+    from tests import CompareTest, OpenTests
 
 
 @task
-def download_core(c, force=False):
-    Core(settings.version if settings.version else input("Please enter version: ")).getting(force=force)
+def download_core(c, force=False, version=None):
+    version = version if version else config.version if config.version else Prompt.ask("Please enter version")
+    Core(version).getting(force=force)
 
 
 @task
-def convert(c, dr=None, ls=False):
-    x2ttester, x2ttester_report = X2ttester(), X2ttesterReport()
-    input_format, output_format = x2ttester.getting_formats(dr)
-    report = x2ttester.conversion_from_files_list(input_format, output_format) if ls else \
-        x2ttester.conversion(input_format, output_format)
-    result_folder = join(StaticData.result_dir(), f"{x2ttester.x2t_version}_{input_format}_{output_format}")
-    x2ttester.copy_result(result_folder, output_format) if not ls else ...
-    x2ttester_report.report_handler(report)
+def conversion_test(c, direction=None, ls=False, telegram=False, version=None):
+    download_core(c, version=version)
+    x2t_version = X2t.version(StaticData.core_dir())
+    conversion = X2tTesterConversion(direction, x2t_version)
+    print(f"[bold green]|INFO| The conversion is running on x2t version: [red]{x2t_version}")
+    report = conversion.from_files_list(config.files_array) if ls else conversion.run()
+    tg_msg = f"Conversion completed on version: `{x2t_version}`\nPlatform: `{HostInfo().os}`" if telegram else None
+    conversion.report.handler(report, x2t_version, tg_msg=tg_msg) if report else print("[red] Report not exists")
+    print(f"[bold red]\n{'-' * 90}\n|INFO| x2t version: {x2t_version}\n{'-' * 90}")
 
 
 @task
-def convert_array(c):
-    conversion, report, tg = X2ttester(), X2ttesterReport(), Telegram()
-    xmllint_report, x2ttester_report = conversion.convert_from_extension_array()
-    print(f"[red]{'-' * 90}\n"f"XMLLINT:\n{report.pandas_read(xmllint_report) if xmllint_report else None}\n")
-    report.report_handler(x2ttester_report)
-    tg.send_document(xmllint_report, caption=f"`XMLLINT REPORT ON VERSION: {conversion.x2t_version}`")
-    tg.send_document(x2ttester_report, caption=f"`CONVERSION REPORT ON VERSION: {conversion.x2t_version}`")
+def make_files(c, telegram=False, direction=None, version=None):
+    download_core(c, version=version)
+    x2t_version = X2t.version(StaticData.core_dir())
+    print(f"[bold green]|INFO| The files will be converted to x2t versions: [red]{x2t_version}")
+    conversion = X2tTesterConversion(direction, x2t_version)
+    report = conversion.run(results_path=True) if direction else conversion.from_extension_json()
+    tg_msg = f"Files are converted to versions: `{x2t_version}`\nPlatform: `{HostInfo().os}`" if telegram else None
+    conversion.report.handler(report, x2t_version, tg_msg=tg_msg) if report else print("[red] Report not exists")
+    print(f"[bold red]\n{'-' * 90}\n|INFO| x2t version: {x2t_version}\n{'-' * 90}")
 
 
 @task
-def xmllint(c, path):
-    lint = XmlLint()
-    lint.run_tests(dir_path=path)
+def compare_test(c, direction: str = None, ls=False, telegram=False):
+    direction = direction if direction else Prompt.ask('Input formats with -', default=None, show_default=False)
+    source_ext, converted_ext = CompareTest().getting_formats(direction)
+    if not source_ext or not converted_ext:
+        raise print('[bold red]|ERROR| The direction is not correct')
+    print("[bold green]|INFO| Starting...")
+    CompareTest().run(
+        FileUtils.get_paths(
+            path=config.converted_docs,
+            extension=converted_ext,
+            dir_include=f"{config.version}_{source_ext}_",
+            names=config.files_array if ls else None,
+            exceptions_files=StaticData.ignore_files,
+            exceptions_dirs=StaticData.ignore_dirs
+        ),
+        source_ext,
+        converted_ext
+    )
+    if telegram:
+        Telegram().send_message(f"Comparison on version {config.version} completed")
 
 
 @task
-def doc_docx(c, st=False, ls=False, df=False, cl=False):
-    StaticData.DOC_ACTIONS = DocActions(source_extension='doc', converted_extension='docx')
-    files_array = StaticData.DOC_ACTIONS.generate_file_array(ls=ls, df=df, cl=cl)
-    comparer = DocDocxCompareImg() if not st else DocDocxStatisticsCompare()
-    comparer.run_compare(files_array) if not st else comparer.run_compare_statistic(files_array)
-    Telegram.send_message('doc-docx comparison completed')
-
-
-@task
-def rtf_docx(c, ls=False, cl=False):
-    StaticData.DOC_ACTIONS = DocActions(source_extension='rtf', converted_extension='docx')
-    comparer = RtfDocxCompareImg()
-    comparer.run_compare(StaticData.DOC_ACTIONS.generate_file_array(ls=ls, cl=cl))
-    Telegram.send_message('rtf-docx comparison completed')
-
-
-@task
-def ppt_pptx(c, ls=False, cl=False):
-    StaticData.DOC_ACTIONS = DocActions(source_extension='ppt', converted_extension='pptx')
-    comparer = PptPptxCompareImg()
-    comparer.run_compare(StaticData.DOC_ACTIONS.generate_file_array(ls=ls, cl=cl))
-    Telegram.send_message('ppt-pptx comparison completed')
-
-
-@task
-def odp_pptx(c, ls=False, cl=False):
-    StaticData.DOC_ACTIONS = DocActions(source_extension='odp', converted_extension='pptx')
-    comparer = OdpPptxCompare()
-    comparer.run_compare(StaticData.DOC_ACTIONS.generate_file_array(ls=ls, cl=cl))
-    Telegram.send_message('odp-pptx comparison completed')
-
-
-@task
-def xls_xlsx(c, st=False, ls=False, cl=False):
-    StaticData.DOC_ACTIONS = DocActions(source_extension='xls', converted_extension='xlsx')
-    files_array = StaticData.DOC_ACTIONS.generate_file_array(ls=ls, cl=cl)
-    comparer = ExcelCompareImage() if not st else StatisticCompare()
-    comparer.run_compare(files_array) if not st else comparer.run_compare_statistic(files_array)
-    Telegram.send_message('xls-xlsx comparison completed')
-
-
-@task
-def opener_pptx(c, odp=False, ppt=False, ls=False):
-    if odp:
-        StaticData.DOC_ACTIONS = DocActions(source_extension='odp', converted_extension='pptx')
-    elif ppt:
-        StaticData.DOC_ACTIONS = DocActions(source_extension='ppt', converted_extension='pptx')
-    else:
-        opener_pptx(c, ppt=True, ls=ls)
-        opener_pptx(c, odp=True, ls=ls)
-        Telegram.send_message('Ppt=>Pptx and Odp => Pptx opening check completed')
-    opener = OpenerPptx()
-    opener.run_opener(StaticData.DOC_ACTIONS.generate_file_array(ls=ls))
-    StaticData.DOC_ACTIONS.create_massage_for_tg(opener.files_with_errors_when_opening, ls=ls)
-
-
-@task
-def opener_docx(c, doc=False, rtf=False, pdf=False, ls=False):
-    if doc:
-        StaticData.DOC_ACTIONS = DocActions(source_extension='doc', converted_extension='docx')
-    elif rtf:
-        StaticData.DOC_ACTIONS = DocActions(source_extension='rtf', converted_extension='docx')
-    elif pdf:
-        StaticData.DOC_ACTIONS = DocActions(source_extension='pdf', converted_extension='docx')
-    else:
-        opener_docx(c, doc=True, ls=ls)
-        opener_docx(c, rtf=True, ls=ls)
-        Telegram.send_message('Doc=>Docx and Rtf=>Docx opening check completed')
-    opener = OpenerDocx()
-    opener.run_opener(StaticData.DOC_ACTIONS.generate_file_array(ls=ls))
-    StaticData.DOC_ACTIONS.create_massage_for_tg(opener.files_with_errors_when_opening, ls=ls)
-
-
-@task
-def opener_xlsx(c, xls=False, ods=False, ls=False):
-    if xls:
-        StaticData.DOC_ACTIONS = DocActions(source_extension='xls', converted_extension='xlsx')
-    elif ods:
-        StaticData.DOC_ACTIONS = DocActions(source_extension='ods', converted_extension='xlsx')
-    else:
-        opener_xlsx(c, xls=True, ls=ls)
-        opener_xlsx(c, ods=True, ls=ls)
-        Telegram.send_message('Xls=>Xlsx and Ods=>Xlsx opening check completed')
-    opener = OpenerXlsx()
-    opener.run_opener(StaticData.DOC_ACTIONS.generate_file_array(ls=ls))
-    StaticData.DOC_ACTIONS.create_massage_for_tg(opener.files_with_errors_when_opening, ls=ls)
-
-
-@task
-def opener_libre(c, ls=False, direction=None, path=''):
-    src, cnv = X2ttester().getting_formats(direction)
-    _path = path if path else settings.converted_docs
-    cnv_extension = [str(cnv)] if cnv else ['ods', 'odp', 'odt']
-    for ext in cnv_extension:
-        OpenerLibre(ext).open_files(
-            FileUtils.get_paths(
-                _path,
-                extension=ext,
-                dir_include=settings.version if not path else None,
-                filename_array=settings.files_array if ls else None
-            )
-        )
-
-
-# Y:/scripts/opencv_documents_comparer/tst/seed_slide_deck_16x9__v1_corn_flower
-@task
-def opener_full(c, cnv=False):
-    if cnv:
-        download_core(c)
-        convert_array(c)
-    opener_pptx(c)
-    opener_docx(c)
-    opener_xlsx(c)
-    opener_libre(c)
-    Telegram.send_message('Full test of the openers completed')
+def open_test(c, direction=None, ls=False, path='', telegram=False, new_test=False, fast_test=False):
+    opener = OpenTests(continue_test=False if fast_test or path or new_test or ls else True)
+    if new_test:
+        warning = f"[red]{'-' * 90}\nThe results will be removed from the report:\n{opener.report.path}\n{'-' * 90}\n"
+        if Prompt.ask(warning, choices=['yes', 'no'], default='no') == 'yes':
+            FileUtils.delete(opener.report.path)
+    source_ext, converted_ext = opener.getting_formats(direction)
+    opener.run(
+        FileUtils.get_paths(
+            path=path if path else config.converted_docs,
+            extension=converted_ext if converted_ext else None,
+            dir_include=f"{config.version}_{source_ext}_" if source_ext else config.version if not path else None,
+            names=config.files_array if ls else None,
+            exceptions_files=StaticData.ignore_files,
+            exceptions_dirs=StaticData.ignore_dirs
+        ),
+        tg_msg=f"Opening test completed on version: {config.version}" if telegram else False
+    )
