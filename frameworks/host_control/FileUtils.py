1bb3b669741f193a45fb44e5ed970c6cadde335a
l02 <l8556@outlook.com>
2023-07-20 14:37:50 +0300

    added host control package

--- frameworks/host_control/FileUtils.py
+++ frameworks/host_control/FileUtils.py
@@ -20,8 +20,8 @@ from .HostInfo import HostInfo
 
 class FileUtils:
     @staticmethod
-    def read_json(path_to_json):
-        with codecs.open(path_to_json, "r", "utf_8_sig") as file:
+    def read_json(path_to_json: str, encoding: str = "utf_8_sig") -> json:
+        with codecs.open(path_to_json, mode="r", encoding=encoding) as file:
             return json.load(file)
 
     @staticmethod
@@ -36,7 +36,7 @@ class FileUtils:
         return path.rstrip(path[-1]) if path[-1] in ['/', '\\'] else path
 
     @staticmethod
-    def make_tmp_file(path: str, tmp_dir: str = '/tmp') -> str:
+    def make_tmp_file(path: str, tmp_dir: str = './tmp') -> str:
         FileUtils.create_dir(tmp_dir) if not exists(tmp_dir) else ...
         tmp_file_path = FileUtils.random_name(tmp_dir, path.split(".")[-1])
         FileUtils.copy(path, tmp_file_path, silence=True)
@@ -61,19 +61,24 @@ class FileUtils:
     def get_paths(
             path: str,
             extension: tuple | str = None,
-            filename_array: list = None,
-            exceptions: list = None,
+            names: list = None,
+            exceptions_files: list = None,
+            exceptions_dirs: list = None,
             dir_include: str = None
     ) -> list:
+        ext_dirs = [join(path, ext_path) for ext_path in exceptions_dirs] if exceptions_dirs else ...
+
         file_paths = []
         for root, dirs, files in walk(path):
             for filename in files:
-                if exceptions and filename in exceptions:
+                if exceptions_files and filename in exceptions_files:
+                    continue
+                if exceptions_dirs and [path for path in ext_dirs if path in root]:
                     continue
                 if dir_include and dir_include not in basename(root):
                     continue
-                if filename_array:
-                    file_paths.append(join(root, filename)) if filename in filename_array else ...
+                if names:
+                    file_paths.append(join(root, filename)) if filename in names else ...
                 elif extension:
                     if filename.lower().endswith(extension if isinstance(extension, tuple) else extension.lower()):
                         file_paths.append(join(root, filename))
@@ -96,7 +101,7 @@ class FileUtils:
 
     @staticmethod
     def last_modified_file(dir_path):
-        files = FileUtils.get_paths(dir_path, exceptions=['.DS_Store'])
+        files = FileUtils.get_paths(dir_path, exceptions_files=['.DS_Store'])
         return max(files, key=getctime) if files else print('[bold red]|WARNING| Last modified file_name not found')
 
     @staticmethod
@@ -107,13 +112,16 @@ class FileUtils:
         return print(f"[bold red]|MOVE WARNING| File not exist: {path_from}")
 
     @staticmethod
-    def create_dir(dir_path, silence=False):
-        if not exists(dir_path):
-            makedirs(dir_path)
-            if isdir(dir_path):
-                return print(f'[green]|INFO| Folder Created: {dir_path}') if not silence else ...
-            return print(f'[bold red]|WARNING| Create folder warning. Folder not created: {dir_path}')
-        print(f'[green]|INFO| Folder exists: {dir_path}') if not silence else ...
+    def create_dir(dir_path: str | tuple, silence=False) -> None:
+        for _dir_path in dir_path if isinstance(dir_path, tuple) else [dir_path]:
+            if not exists(_dir_path):
+                makedirs(_dir_path)
+                if isdir(_dir_path):
+                    print(f'[green]|INFO| Folder Created: {_dir_path}') if not silence else ...
+                    continue
+                print(f'[bold red]|WARNING| Create folder warning. Folder not created: {_dir_path}')
+                continue
+            print(f'[green]|INFO| Folder exists: {_dir_path}') if not silence else ...
 
     @staticmethod
     def unpacking_7zip(archive_path, execute_path, delete=False):
@@ -150,21 +158,26 @@ class FileUtils:
         FileUtils.delete(archive_path) if delete_archive else ...
 
     @staticmethod
-    def delete(path: str, all_from_folder: bool = False, silence: bool = False) -> None:
-        if not exists(path):
-            return print(f"[bold red]|DELETE WARNING| Path not exist: {path}") if not silence else ...
-        if isdir(path):
-            rmtree(path, ignore_errors=True)
-            if all_from_folder:
-                FileUtils.create_dir(path, silence=True)
-                if any(scandir(path)):
-                    return print(f"[bold red]|DELETE WARNING| Not all files are removed from directory: {path}")
-                return print(f'[green]|INFO| Folder is cleared') if not silence else ...
-        elif isfile(path):
-            remove(path)
-        if exists(path):
-            return print(f"[bold red]|DELETE WARNING| Folder is not deleted: {path}")
-        print(f'[green]|INFO| Deleted: {path}') if not silence else ...
+    def delete(path: str | tuple, all_from_folder: bool = False, silence: bool = False) -> None:
+        for _path in path if isinstance(path, tuple) else [path]:
+            if not exists(_path):
+                print(f"[bold red]|DELETE WARNING| Path not exist: {_path}") if not silence else ...
+                continue
+            if isdir(_path):
+                rmtree(_path, ignore_errors=True)
+                if all_from_folder:
+                    FileUtils.create_dir(_path, silence=True)
+                    if any(scandir(_path)):
+                        print(f"[bold red]|DELETE WARNING| Not all files are removed from directory: {_path}")
+                        continue
+                    print(f'[green]|INFO| Folder is cleared') if not silence else ...
+                    continue
+            elif isfile(_path):
+                remove(_path)
+            if exists(_path):
+                print(f"[bold red]|DELETE WARNING| Folder is not deleted: {_path}")
+                continue
+            print(f'[green]|INFO| Deleted: {_path}') if not silence else ...
 
     @staticmethod
     def random_string(path_to_dir, chars=string.ascii_uppercase + string.digits, num_chars=50, extension=None):
@@ -177,8 +190,8 @@ class FileUtils:
     @staticmethod
     def random_name(path, extension=None) -> str:
         while True:
-            random_name = f'{randint(500, 50000)}.{extension}' if extension else f'{randint(500, 50000)}'
-            random_path = join(path, random_name)
+            name = f'{randint(500, 50000)}.{extension.replace(".", "")}' if extension else f'{randint(500, 50000)}'
+            random_path = join(path, name)
             if not exists(random_path):
                 return random_path
 
@@ -186,9 +199,10 @@ class FileUtils:
     def fix_double_folder(dir_path):
         path = join(dir_path, basename(dir_path))
         if isdir(path):
-            for file in track(listdir(path), description='[green]Fixing the double folder...'):
+            for file in listdir(path):
                 FileUtils.move(join(path, file), join(dir_path, file))
             if not any(scandir(path)):
+                print("[green]|INFO| Fixed double folder")
                 return FileUtils.delete(path)
             print("[red]|WARNING| Not all objects are moved")
 
@@ -201,13 +215,14 @@ class FileUtils:
         return False
 
     @staticmethod
-    def download_file(url: str, dir_path: str, name: str = None):
-        FileUtils.create_dir(dir_path)
-        file_path = join(dir_path, name if name else basename(url))
+    def download_file(url: str, dir_path: str, name: str = None) -> None:
+        FileUtils.create_dir(dir_path, silence=True)
+        _name = name if name else basename(url)
+        file_path = join(dir_path, _name)
         with get(url, stream=True) as r:
             r.raise_for_status()
             with open(file_path, 'wb') as file:
-                for chunk in track(r.iter_content(chunk_size=1024 * 1024), description=f'[red] Downloading: {name}'):
+                for chunk in track(r.iter_content(chunk_size=1024 * 1024), description=f'[red] Downloading: {_name}'):
                     if chunk:
                         file.write(chunk)
         print(f"[bold green]|INFO| File Saved to: {file_path}" if isfile(file_path) else f"[red]|WARNING| Not exist")
@@ -215,11 +230,11 @@ class FileUtils:
             print(f"[red]|WARNING| Size different\nFile:{getsize(file_path)}\nOn server:{r.headers['Content-Length']}")
 
     @staticmethod
-    def find_in_line_by_key(text, key_trigger, split_by='\n', separator=':'):
+    def find_in_line_by_key(text: str, key: str, split_by: str = '\n', separator: str = ':') -> str | None:
         for line in text.split(split_by):
             if separator in line:
-                key, value = line.strip().split(separator, 1)
-                if key.lower() == key_trigger.lower():
+                _key, value = line.strip().split(separator, 1)
+                if _key.lower() == key.lower():
                     return value.strip()
 
     @staticmethod
@@ -250,7 +265,7 @@ class FileUtils:
                     try:
                         process.terminate()
                     except Exception as e:
-                        print(f'|Warning| Exception when terminate process: {e}, process: {terminate_process}')
+                        print(f'[bold red]|Warning| Exception when terminate process {terminate_process}: {e}')
 
     @staticmethod
     def run_command(command):
--- frameworks/host_control/__init__.py
+++ frameworks/host_control/__init__.py
@@ -2,3 +2,6 @@
 from .FileUtils import FileUtils
 from .key_actions import KeyActions
 from .HostInfo import HostInfo
+
+if HostInfo().os == 'windows':
+    from .windows_handler import Window
