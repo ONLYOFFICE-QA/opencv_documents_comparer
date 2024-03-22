3f391d878db58fed3ffe039ab10e4616f03923ba
l02 <l8556@outlook.com>
2024-03-22 13:19:02 +0300

    Added docstrings for UrlGenerator.py

--- frameworks/editors/onlyoffice/onlyoffice_x2t_libs/UrlGenerator.py
+++ frameworks/editors/onlyoffice/onlyoffice_x2t_libs/UrlGenerator.py
@@ -4,7 +4,21 @@ from ..handlers.VersionHandler import VersionHandler
 
 
 class UrlGenerator(VersionHandler):
-    def __init__(self, version):
+    """
+    Class for generating download URLs based on the version, operating system, and architecture.
+
+    Inherits from VersionHandler to handle version-specific logic.
+
+    Attributes:
+        host (str): Base URL host.
+        url_version (str): Version part of the URL.
+        branch (str): Branch of the repository.
+    """
+    def __init__(self, version: str):
+        """
+        Initializes the UrlGenerator object.
+        :param version: Version of the core.
+        """
         super().__init__(version)
         self.host = 'https://s3.eu-west-1.amazonaws.com/repo-doc-onlyoffice-com'
         self.url_version = f"v{self.major_version}.{self.minor_version}"
@@ -12,12 +26,20 @@ class UrlGenerator(VersionHandler):
         self.branch = self.branch()
 
     @property
-    def url(self):
+    def url(self) -> str:
+        """
+        Generates the download URL.
+        :return: Download URL for the core.
+        """
         if self.branch == 'develop':
             return f"{self.host}/{self.os}/core/{self.branch}/{self.url_build}/{self.arch}/core.7z"
         return f"{self.host}/{self.os}/core/{self.branch}/{self.url_version}/{self.url_build}/{self.arch}/core.7z"
 
     def branch(self) -> str:
+        """
+        Determines the branch based on the version.
+        :return: Branch name.
+        """
         if "99.99.99" in self.version:
             return 'develop'
         elif self.minor_version != '0':
@@ -25,12 +47,20 @@ class UrlGenerator(VersionHandler):
         return "release"
 
     def url_build(self) -> str:
+        """
+        Generates the URL build string.
+        :return: URL build string.
+        """
         if self.os == 'windows':
             return self.version
         return f"{self.major_version}.{self.minor_version}-{self.build}"
 
     @property
     def arch(self) -> str:
+        """
+        Returns the architecture string based on the operating system.
+        :return: Architecture string.
+        """
         arch = HostInfo().arch
         if self.os == 'linux':
             return arch.replace("x86_", "x")
@@ -41,6 +71,10 @@ class UrlGenerator(VersionHandler):
 
     @property
     def os(self) -> str:
+        """
+        Returns the operating system string.
+        :return: Operating system string.
+        """
         os_info = HostInfo().os
         if os_info.lower() in ['linux', 'mac', 'windows']:
             return os_info.lower()
