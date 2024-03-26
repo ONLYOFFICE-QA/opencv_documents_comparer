84c7035c7d8a67aaea99f3d7fc16247924bd8565
l02 <l8556@outlook.com>
2024-03-26 15:06:34 +0300

    minor refactoring

--- frameworks/editors/onlyoffice/onlyoffice_x2t_libs/x2t_libs_xml.py
+++ frameworks/editors/onlyoffice/onlyoffice_x2t_libs/x2t_libs_xml.py
@@ -26,25 +26,12 @@ class X2tLibsXML:
         self.x2t_dir = x2t_dir
         self.x2t_lib_config = File.read_json(join(dirname(realpath(__file__)), 'assets', 'x2t_libs_paths.json'))
 
-    def generate_doc_renderer_config(self):
-        """
-        Generates the document renderer configuration XML.
-        :return: XML element containing document renderer configuration.
-        """
-        root = ET.Element("Settings")
-        ET.SubElement(root, "file").text = self.x2t_lib_config['native']
-        ET.SubElement(root, "file").text = self.x2t_lib_config['jquery_native']
-        ET.SubElement(root, "allfonts").text = self.x2t_lib_config['AllFonts']
-        ET.SubElement(root, "file").text = self.x2t_lib_config['xregexp_all_min']
-        ET.SubElement(root, "sdkjs").text = self.x2t_lib_config['sdkjs']
-        return root
-
-    def create_doc_renderer_config(self):
+    def create_doc_renderer_config(self) -> None:
         """
         Creates the document renderer configuration XML file.
         :return: Path to the created XML file.
         """
-        self._create(self.generate_doc_renderer_config(), xml_path=join(self.x2t_dir, 'DoctRenderer.config'))
+        self._create(self._generate_doc_renderer_config(), xml_path=join(self.x2t_dir, 'DoctRenderer.config'))
         print("[green]|INFO| DocRenderer.confing created")
 
     @staticmethod
@@ -59,3 +46,16 @@ class X2tLibsXML:
         ET.indent(tree, '  ')
         tree.write(xml_path, encoding="UTF-8", xml_declaration=True)
         return xml_path
+
+    def _generate_doc_renderer_config(self) -> ET.Element:
+        """
+        Generates the document renderer configuration XML.
+        :return: XML element containing document renderer configuration.
+        """
+        root = ET.Element("Settings")
+        ET.SubElement(root, "file").text = self.x2t_lib_config['native']
+        ET.SubElement(root, "file").text = self.x2t_lib_config['jquery_native']
+        ET.SubElement(root, "allfonts").text = self.x2t_lib_config['AllFonts']
+        ET.SubElement(root, "file").text = self.x2t_lib_config['xregexp_all_min']
+        ET.SubElement(root, "sdkjs").text = self.x2t_lib_config['sdkjs']
+        return root
--- frameworks/xmllint/XmllintReport.py
+++ frameworks/xmllint/XmllintReport.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 from os.path import join
-
 from frameworks.StaticData import StaticData
 from frameworks.decorators import singleton
 from host_tools import HostInfo, Dir
 from frameworks.editors.onlyoffice import VersionHandler, X2t
 from frameworks.report import Report
 
-
 @singleton
 class XmllintReport(Report):
     def __init__(self):
         super().__init__()
         self.report_dir = StaticData.reports_dir()
-        self.time_pattern = f"{datetime.now().strftime('%H_%M_%S')}"
+        self.time_pattern = datetime.now().strftime('%H_%M_%S')
         self.x2t_dir = StaticData.core_dir()
 
     def report_path(self):
