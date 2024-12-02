0f4f9f5e9b3e2a3be52f396eae7f30a420cf1870
l02 <l8556@outlook.com>
2024-12-02 16:00:33 +0300

    Added a new file for quick check

--- frameworks/editors/onlyoffice/x2ttester/X2tTesterConfig.py
+++ frameworks/editors/onlyoffice/x2ttester/X2tTesterConfig.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
+from rich import print
+
+from frameworks.decorators import highlighter
+
 
 class X2tTesterConfig(BaseModel):
     input_dir: str
@@ -16,3 +20,16 @@ class X2tTesterConfig(BaseModel):
     trough_conversion: bool = False
     environment_off: bool = False
     out_x2ttester_param: bool = False
+
+    @model_validator(mode="after")
+    def set_computed_fields(self):
+        if self.out_x2ttester_param:
+            self._out_param()
+        return self
+
+    @highlighter()
+    def _out_param(self):
+        print("[magenta]|INFO| X2ttester Parameters:")
+        for field, value in self.model_dump().items():
+            if field != 'out_x2ttester_param':
+                print(f"[green]{field}: [cyan]{value}[/]")
