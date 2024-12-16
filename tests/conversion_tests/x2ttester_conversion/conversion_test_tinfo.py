edad7c36b71c0b29853c785beec892b7157ff18e
l02 <l8556@outlook.com>
2024-12-16 14:48:58 +0300

    minor refactoring

--- tests/conversion_tests/x2ttester_conversion/conversion_test_tinfo.py
+++ tests/conversion_tests/x2ttester_conversion/conversion_test_tinfo.py
@@ -14,6 +14,7 @@ class ConversionTestInfo:
         self.x2t_version = self.config.x2t_version
         self.quick_check = self.config.quick_check
         self.env_off = self.config.environment_off
+        self.line = '-' * 90
 
     def out_test_info(self, mode: str):
         print(
@@ -34,15 +35,20 @@ class ConversionTestInfo:
         )
 
         if out:
-            print(f"[green]{'-' * 90}\n|INFO|{msg}\n{'-' * 90}")
+            print(f"[green]{self.line}\n|INFO|{msg}\n{self.line}")
 
         return msg
 
-    def get_make_files_result_msg(self, version: str, t_format: bool):
-        return (
+    def get_make_files_result_msg(self, version: str, t_format: bool, out: bool = True):
+        msg = (
             f"Files for open test converted\n"
             f"Version: `{version}`\n"
             f"X2t version: `{self.x2t_version}`\n"
             f"Platform: `{HostInfo().os}`\n"
             f"Mode: `{'t-format' if t_format else 'Default'}`"
         )
+
+        if out:
+            print(f"[green]{self.line}\n|INFO|{msg}\n{self.line}")
+
+        return msg
