16ea8b6010d0508feaa57c692c26b0647b0a5bc8
Nikolai Lambrov <nikolay.lambrov@onlyoffice.com>
2026-02-05 19:25:23 +0300

    Update __all__ exports in tests/__init__.py to dynamically include CompareTest and OpenTests for Windows. This change enhances module accessibility based on the operating system.

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,8 +2,9 @@
 from host_tools import HostInfo
 from .conversion_tests import X2tTesterConversion, X2ttesterTestConfig, ErrorHandler
 
+__all__ = ['X2tTesterConversion', 'X2ttesterTestConfig', 'ErrorHandler']
+
 if HostInfo().os == 'windows':
     from .compare_tests import CompareTest
     from .open_tests import OpenTests
-
-__all__ = ['X2tTesterConversion', 'X2ttesterTestConfig', 'ErrorHandler', 'CompareTest', 'OpenTests']
+    __all__.extend(['CompareTest', 'OpenTests'])
